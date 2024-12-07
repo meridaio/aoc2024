b1a42d56ccfebf3bbed70a71dd3ce4fcb6d092e7
Nathan George <ntg9vz@virginia.edu>
2024-12-07 11:15:41 -0500

    part 2 stuff from yesterday

--- day6.go
+++ day6.go
@@ -5,7 +5,14 @@ import (
 	"slices"
 )
 
-func step(matrix [][]rune, visited [][]bool) [][]rune {
+type PointHistory struct {
+	Up    bool
+	Down  bool
+	Right bool
+	Left  bool
+}
+
+func step(matrix [][]rune, visited [][]bool, pointHistory [][]PointHistory) (newMatrix [][]rune, loopDetected bool) {
 	for x, row := range matrix {
 		up := slices.Index(row, '^')
 		down := slices.Index(row, 'v')
@@ -15,89 +22,124 @@ func step(matrix [][]rune, visited [][]bool) [][]rune {
 		if up > 0 {
 			newX := x - 1
 
-			log.Printf("moving up from (%d, %d) to (%d, %d)", x, up, newX, up)
+			//log.Printf("moving up from (%d, %d) to (%d, %d)", x, up, newX, up)
 			visited[x][up] = true
-			if newX >= len(matrix) {
-				log.Printf("out of bounds")
-				return nil
+			if newX < 0 {
+				//log.Printf("out of bounds")
+				return nil, false
+			}
+			if pointHistory[x][up].Up {
+				return nil, true
+			} else {
+				pointHistory[x][up].Up = true
 			}
 			if matrix[newX][up] == '#' {
-				log.Printf("Encountered obstacle, turning right")
+				//log.Printf("Encountered obstacle, turning right")
 				matrix[x][up] = '>'
-				return matrix
+				return matrix, false
 			}
 			matrix[newX][up] = matrix[x][up]
 			matrix[x][up] = '.'
-			return matrix
+			return matrix, false
 		} else if down > 0 {
 			newX := x + 1
 
-			log.Printf("moving down from (%d, %d) to (%d, %d)", x, down, newX, down)
+			//log.Printf("moving down from (%d, %d) to (%d, %d)", x, down, newX, down)
 			visited[x][down] = true
-			if newX < 0 {
-				log.Printf("out of bounds")
-				return nil
+			if newX >= len(matrix) {
+				//log.Printf("out of bounds")
+				return nil, false
+			}
+			if pointHistory[x][down].Down {
+				return nil, true
+			} else {
+				pointHistory[x][down].Down = true
 			}
 			if matrix[newX][down] == '#' {
-				log.Printf("Encountered obstacle, turning left")
+				//log.Printf("Encountered obstacle, turning left")
 				matrix[x][down] = '<'
-				return matrix
+				return matrix, false
 			}
 			matrix[newX][down] = matrix[x][down]
 			matrix[x][down] = '.'
-			return matrix
+			return matrix, false
 		} else if right > 0 {
 			newY := right + 1
 
-			log.Printf("moving right from (%d, %d) to (%d, %d)", x, right, x, newY)
+			//log.Printf("moving right from (%d, %d) to (%d, %d)", x, right, x, newY)
 			visited[x][right] = true
 			if newY >= len(matrix[0]) {
-				log.Printf("out of bounds")
-				return nil
+				//log.Printf("out of bounds")
+				return nil, false
+			}
+			if pointHistory[x][right].Right {
+				return nil, true
+			} else {
+				pointHistory[x][right].Right = true
 			}
 			if matrix[x][newY] == '#' {
-				log.Printf("Encountered obstacle, turning down")
+				//log.Printf("Encountered obstacle, turning down")
 				matrix[x][right] = 'v'
-				return matrix
+				return matrix, false
 			}
 			matrix[x][newY] = matrix[x][right]
 			matrix[x][right] = '.'
-			return matrix
+			return matrix, false
 		} else if left > 0 {
 			newY := left - 1
 
-			log.Printf("moving left from (%d, %d) to (%d, %d)", x, left, x, newY)
+			//log.Printf("moving left from (%d, %d) to (%d, %d)", x, left, x, newY)
 			visited[x][left] = true
 			if newY < 0 {
-				log.Printf("out of bounds")
-				return nil
+				//log.Printf("out of bounds")
+				return nil, false
+			}
+			if pointHistory[x][left].Left {
+				return nil, true
+			} else {
+				pointHistory[x][left].Left = true
 			}
 			if matrix[x][newY] == '#' {
-				log.Printf("Encountered obstacle, turning up")
+				//log.Printf("Encountered obstacle, turning up")
 				matrix[x][left] = '^'
-				return matrix
+				return matrix, false
 			}
 			matrix[x][newY] = matrix[x][left]
 			matrix[x][left] = '.'
-			return matrix
+			return matrix, false
 		} else {
 			continue
 		}
 	}
-	return nil
+	return nil, false
 }
 
 func walkMatrix(matrix [][]rune) [][]bool {
 	visited := make([][]bool, len(matrix))
+	pointHistory := make([][]PointHistory, len(matrix))
 	for x, row := range matrix {
 		visited[x] = make([]bool, len(row))
+		pointHistory[x] = make([]PointHistory, len(row))
 	}
 	t := 0
 	for matrix != nil {
 		t = t + 1
-		matrix = step(matrix, visited)
+		var loopDetected bool
+		matrix, loopDetected = step(matrix, visited, pointHistory)
+		if loopDetected {
+			return nil
+		}
 	}
 	return visited
+}
+
+func copyMatrix(matrix [][]rune) [][]rune {
+	matrixCopy := make([][]rune, len(matrix))
+	for x, v := range matrix {
+		matrixCopy[x] = make([]rune, len(matrix))
+		copy(matrixCopy[x], v)
+	}
+	return matrixCopy
 }
 
 func Day6() {
@@ -111,6 +153,8 @@ func Day6() {
 		}
 		matrix[x] = row
 	}
+	originalMatrix := copyMatrix(matrix)
+
 	walked := walkMatrix(matrix)
 
 	sum := 0
@@ -121,5 +165,26 @@ func Day6() {
 			}
 		}
 	}
+
+	p2Sum := 0
+	for i, v := range walked {
+		for j, b := range v {
+			if b {
+				m := copyMatrix(originalMatrix)
+				log.Printf("(%d, %d)", i, j)
+				if m[i][j] == '.' {
+					m[i][j] = '#'
+
+					w := walkMatrix(m)
+					if w == nil {
+						//log.Print("loop detected")
+						p2Sum = p2Sum + 1
+					}
+				}
+			}
+		}
+	}
+
 	log.Printf("Part 1: %d", sum)
+	log.Printf("Part 2: %d", p2Sum)
 }
